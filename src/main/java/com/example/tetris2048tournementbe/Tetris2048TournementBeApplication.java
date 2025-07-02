96c7894ed7fc026cfa142bb223c3fb4140b4bbc7
Halil Sağlam <halil04saglam@gmail.com>
2025-07-02 12:15:24 +0300

    init

---
+++ src/main/java/com/example/tetris2048tournementbe/Tetris2048TournementBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tetris2048tournementbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tetris2048TournementBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tetris2048TournementBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tetris2048tournementbe/Tetris2048TournementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tetris2048tournementbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tetris2048TournementBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
